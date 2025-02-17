4a6f69516e300372dc7a8524b30c260d70033fa8
Kristijan Miloseski <kristijan.miloseski.f@gmail.com>
2025-02-17 22:52:45 +0100

    Init

--- src/main/java/com/example/webservices/WebServicesApplication.java
+++ src/main/java/com/example/webservices/WebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebServicesApplication {
 
 	public static void main(String[] args) {
+		//Main method to run the application
 		SpringApplication.run(WebServicesApplication.class, args);
 	}
 
